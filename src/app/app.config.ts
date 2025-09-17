c71e82f92a9939cd822e51cc0afdff657d400a3e
Tobias Dreifke <toobsecret@gmail.com>
2025-09-17 12:16:54 +0200

    added firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "join-e5bef", appId: "1:387219287108:web:b0b9a44f23a5141e8a2011", storageBucket: "join-e5bef.firebasestorage.app", apiKey: "AIzaSyCHwzQJ3d8q8bspT7nv86740Jz4OFdu9xg", authDomain: "join-e5bef.firebaseapp.com", messagingSenderId: "387219287108" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
