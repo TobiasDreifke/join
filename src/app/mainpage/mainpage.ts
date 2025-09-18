8489adbacc5ebb3e64ba65b6cc0e0387b2084879
Tobias Dreifke <toobsecret@gmail.com>
2025-09-18 15:59:00 +0200

    placed the popup inside main for visibility

--- src/app/mainpage/mainpage.ts
+++ src/app/mainpage/mainpage.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Contacts } from './contacts/contacts';
 import { Sidebar } from "../shared/sidebar/sidebar";
+import { AddContact } from "./contacts/add-contact/add-contact";
 
 @Component({
   selector: 'app-mainpage',
-  imports: [Contacts, Sidebar],
+  imports: [Contacts, Sidebar, AddContact],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.scss'
 })
