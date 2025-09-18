9b80f2992b020b011e1da006b897ca07affd273e
Demis Moysidis <demis.moysidis@outlook.de>
2025-09-18 16:32:48 +0200

    feat: add contact & delete contact & update contact in contact service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './shared/sidebar/sidebar';
+import { ContactService } from './services/contact-service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,10 @@ import { Sidebar } from './shared/sidebar/sidebar';
 })
 export class App {
   protected readonly title = signal('join');
+
+  contactData = inject(ContactService);
+
+  constructor(){
+    console.log(this.contactData.contactsList)
+  }
 }
--- src/app/services/contact-service.ts
+++ src/app/services/contact-service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable, OnDestroy } from '@angular/core';
-import { collection, Firestore, onSnapshot } from '@angular/fire/firestore';
+import { addDoc, collection, deleteDoc, doc, Firestore, onSnapshot, orderBy, query, updateDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +11,49 @@ export class ContactService implements OnDestroy{
   contactsList: Contact[] = [];
 
   constructor(){
+    this.unsubContactsList = this.getContactData();
+  }
 
-    this.unsubContactsList = onSnapshot(this.getContactsRef(), (contactsList) => {
+  getContactData(){
+    const q = query(this.getContactsRef(), orderBy("name"))
+    return onSnapshot(q, (contactsList) => {
       contactsList.forEach((contact)=> {
-        this.contactsList.push(this.setObjectTypeToContact(contact.data()))
+        this.contactsList.push(this.setObjectTypeToContact(contact.data(), contact.id))
       })
     })
+  }
 
+  async addContact(contact: Contact){
+    await addDoc(this.getContactsRef(), contact);
+  }
+
+  async deleteContact(contactId: string){
+    await deleteDoc(this.getSingleContactRef(contactId));
+  }
+
+  async updateContact(contactId: string, contactData: Contact){
+    await updateDoc(this.getSingleContactRef(contactId), this.getCleanJson(contactData))
   }
 
   getContactsRef(){
     return collection(this.firestore, 'contact');
   }
 
-  setObjectTypeToContact(obj: any): Contact{
+  getSingleContactRef(docId: string){
+    return doc(collection(this.firestore, 'contact'), docId)
+  }
+
+  getCleanJson(obj: Contact){
     return {
+      name: obj.name,
+      email: obj.email,
+      phone: obj.phone
+    }
+  }
+
+  setObjectTypeToContact(obj: any, id: string): Contact{
+    return {
+      id: id,
       name: obj.name,
       email: obj.email,
       phone: obj.phone
