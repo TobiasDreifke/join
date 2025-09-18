be2d6ae053ad8e15a7c8b428eaa9f71404a960d6
Tobias Dreifke <toobsecret@gmail.com>
2025-09-18 14:28:26 +0200

    routing done, contact basic structure

--- src/app/mainpage/mainpage.ts
+++ src/app/mainpage/mainpage.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Contacts } from './contacts/contacts';
+import { Sidebar } from "../shared/sidebar/sidebar";
 
 @Component({
   selector: 'app-mainpage',
-  imports: [Contacts],
+  imports: [Contacts, Sidebar],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.scss'
 })
