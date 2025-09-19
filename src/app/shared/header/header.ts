37550f886ad9733330347453821b58d54475d0a1
Demis Moysidis <demis.moysidis@outlook.de>
2025-09-19 12:26:48 +0200

    feat: header toggle function

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,20 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  notVisible = false;
 
+  toggleUserMenu(){
+    if(this.notVisible == true){
+      this.notVisible = false;
+    }else{
+      this.notVisible = true;
+    }
+  }
 }
