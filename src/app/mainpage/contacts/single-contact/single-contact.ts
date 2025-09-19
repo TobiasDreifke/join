b352fcd341d2d5207e4b12753e7293ebf80cefc3
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-19 16:19:08 +0200

    add toggleMenu function and with responsive connation

--- src/app/mainpage/contacts/single-contact/single-contact.ts
+++ src/app/mainpage/contacts/single-contact/single-contact.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-single-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-contact.html',
   styleUrl: './single-contact.scss'
 })
 export class SingleContact {
+  isMenuOpen = false;
+
+toggleMenu() {
+  this.isMenuOpen = !this.isMenuOpen;
+}
+
 
 }
