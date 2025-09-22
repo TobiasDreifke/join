6fc1ea57cf29634e178461819a534a3ae7c04673
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-22 13:14:54 +0200

    add handleClick function && Edit ts

--- src/app/mainpage/contacts/single-contact/single-contact.ts
+++ src/app/mainpage/contacts/single-contact/single-contact.ts
@@ -1,20 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import {Component,ElementRef,EventEmitter,HostListener,Output,} from '@angular/core';
 
 @Component({
   selector: 'app-single-contact',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './single-contact.html',
-  styleUrl: './single-contact.scss'
+  styleUrl: './single-contact.scss',
 })
 export class SingleContact {
   @Output() edit = new EventEmitter<void>();
 
   isMenuOpen = false;
+  hoverActive = false;
+
+
+  constructor(private elementRef: ElementRef) {}
 
 toggleMenu() {
   this.isMenuOpen = !this.isMenuOpen;
+
+  if (!this.isMenuOpen) {
+    this.hoverActive = true;
+    setTimeout(() => {
+      this.hoverActive = false;
+    }, 300); 
+  }
 }
+  
+  @HostListener('document:click', ['$event'])
+  handleClick(event: MouseEvent) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
 
-
+    if (this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
 }
