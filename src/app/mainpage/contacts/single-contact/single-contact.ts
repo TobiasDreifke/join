2d94ab2c86b135e0a823c03c19c31b1ad1e42d4f
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-24 14:45:39 +0200

    add edite for getInitials function

--- src/app/mainpage/contacts/single-contact/single-contact.ts
+++ src/app/mainpage/contacts/single-contact/single-contact.ts
@@ -70,13 +70,17 @@ deleteElements() {
 }
 
 
-  getInitials(name?: string): string {
-  if (!name) return '';
-  return name
-    .split(' ')
-    .map(word => word[0])
-    .join('')
-    .toUpperCase();
-}
+
+getInitials(name?: string): string {
+    if (!name) return '';
+    const parts = name.trim().split(' ').filter(p => p);
+    if (parts.length <= 2) {
+      return parts.map(p => p[0].toUpperCase()).join('');
+    } else {
+      return (parts[0][0] + parts[parts.length - 1][0]).toUpperCase();
+    }
+  } 
+
+
 
 }
