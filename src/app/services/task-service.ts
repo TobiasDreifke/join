07b76ad6dadd655d75bc5ced4bd15001f079db75
Tobias Dreifke <toobsecret@gmail.com>
2025-09-25 17:08:34 +0200

    created taskservice

--- src/app/services/task-service.ts
+++ src/app/services/task-service.ts
@@ -1,8 +1,45 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
+import { inject, Injectable } from '@angular/core';
+import { addDoc, collection, Firestore, onSnapshot } from '@angular/fire/firestore';
+import { TaskInterface } from '../interfaces/tasks.interface';
+; @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  
+  firestore: Firestore = inject(Firestore);
+  unsubscribeTasksList;
+  tasksList: TaskInterface[] = [];
+
+  constructor() {
+    this.unsubscribeTasksList = onSnapshot(collection(this.firestore, "tasks"), (tasksObject) => {
+      this.tasksList = [];
+      tasksObject.forEach((element) => {
+        console.log(element.id, element.data());
+        this.tasksList.push(this.setTaskObject(element.id, element.data() as TaskInterface))
+      });
+    });
+  }
+
+  async addDocumentToServer(task: TaskInterface) {
+    await addDoc(collection(this.firestore, "tasks"), task);
+  }
+
+  setTaskObject(id: string, obj: TaskInterface, assignedContacts: Contact[] = []): TaskInterface {
+    return {
+      id: id,
+      title: obj.title,
+      description: obj.description,
+      due_date: obj.due_date,
+      priority: obj.priority,
+      category: obj.category,
+      stage: obj.stage,
+      subtask: obj.subtask || [],
+      assigned_to: assignedContacts
+    };
+
+  }
+  ngOnDestroy() {
+    if (this.unsubscribeTasksList) {
+      this.unsubscribeTasksList();
+    }
+  }
 }
