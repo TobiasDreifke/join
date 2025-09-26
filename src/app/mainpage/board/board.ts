492af742247bf1e5c0db39ca4978ba47c0ef29d1
Tobias Dreifke <toobsecret@gmail.com>
2025-09-26 13:51:54 +0200

    single components are now routed to main

--- src/app/mainpage/board/board.ts
+++ src/app/mainpage/board/board.ts
@@ -2,14 +2,23 @@ import { Component, inject, Input } from '@angular/core';
 import { TaskService } from '../../services/task-service';
 import { ContactService } from '../../services/contact-service';
 import { CommonModule } from '@angular/common';
-import { NgModel } from '@angular/forms';
+import { OverviewTasks } from './overview-tasks/overview-tasks';
+import { SearchbarHeader } from './searchbar-header/searchbar-header';
+import { SingleTaskPopup } from './single-task-popup/single-task-popup';
 @Component({
   selector: 'app-board',
-  imports: [CommonModule],
+  imports: [CommonModule, OverviewTasks, SearchbarHeader, SingleTaskPopup],
   templateUrl: './board.html',
   styleUrl: './board.scss'
 })
 export class Board {
+
+
+
+
+
+  
+  // ↓↓↓↓↓↓↓↓↓↓↓ HOW TO INJECT BOTH SERVICES ↓↓↓↓↓↓↓↓↓↓↓
   taskService = inject(TaskService)
   contactService = inject(ContactService);
 
