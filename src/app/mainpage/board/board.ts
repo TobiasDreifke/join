696d29d5dcf57148c85e09e463498e6d71bc9498
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-26 09:12:37 +0200

    Add edite for board

--- src/app/mainpage/board/board.ts
+++ src/app/mainpage/board/board.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { SearchbarHeader } from './searchbar-header/searchbar-header';
 
 @Component({
   selector: 'app-board',
-  imports: [SearchbarHeader],
   templateUrl: './board.html',
   styleUrl: './board.scss'
 })
