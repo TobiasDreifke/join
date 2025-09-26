aeda8ca69060f202035af0f399cc093f89dabd95
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-26 09:19:48 +0200

    add edit for board

--- src/app/mainpage/board/board.ts
+++ src/app/mainpage/board/board.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchbarHeader } from './searchbar-header/searchbar-header';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.html',
+  imports: [SearchbarHeader],
   styleUrl: './board.scss'
 })
 export class Board {
