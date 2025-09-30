652de65f8e05e15e1a04c09c536f6c4bc91bb4e3
KC-JulianKohn <julian.kohn@hotmail.de>
2025-09-30 16:02:25 +0200

    add first funktions

--- src/app/mainpage/tasks/tasks.ts
+++ src/app/mainpage/tasks/tasks.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { TaskService } from '../../services/task-service';
+import { ContactService } from '../../services/contact-service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,5 +10,18 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './tasks.scss'
 })
 export class Tasks {
+  taskService = inject(TaskService)
+  contactService = inject(ContactService);
 
+  contactId: string | null = null;
+
+  constructor() {
+    if (this.contactService.contactsList.length > 0) {
+      this.contactId = this.contactService.contactsList[0].id || null;
+    }
+  }
+
+  get contact(): Contact | undefined {
+    return this.contactService.contactsList.find(c => c.id === this.contactId);
+  }
 }
