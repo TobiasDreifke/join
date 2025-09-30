7930c7202eedd5a6995cd9966b1d55fb804ff6ea
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-09-30 11:52:14 +0200

    Add active in interface && add toggleActive and getInitials function

--- src/app/interfaces/contacts.interface.ts
+++ src/app/interfaces/contacts.interface.ts
@@ -1,7 +1,9 @@
 interface Contact {
+
     id?: string,
     name: string,
     email: string,
     phone: string,
-    initial_avatar_color?: string
+    initial_avatar_color?: string,
+    active?: boolean
 }
--- src/app/mainpage/board/single-task-popup/single-task-popup.ts
+++ src/app/mainpage/board/single-task-popup/single-task-popup.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ContactService } from '../../../services/contact-service';
+import { TaskService } from '../../../services/task-service';
 
 @Component({
   selector: 'app-single-task-popup',
@@ -7,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './single-task-popup.scss'
 })
 export class SingleTaskPopup {
+    taskService = inject(TaskService)
+  contactService = inject(ContactService);
 
+  contactId: string | null = null;
+
+  constructor() {
+    if (this.contactService.contactsList.length > 0) {
+      this.contactId = this.contactService.contactsList[0].id || null;
+    }
+  }
+  
+  get contact(): Contact | undefined {
+    return this.contactService.contactsList.find(c => c.id === this.contactId);
+  }
+
+  
+  toggleActive(item: any) {
+    item.active = !item.active;
+  }
+  
+  getInitials(name?: string): string {
+    if (!name) return '';
+    const parts = name.trim().split(' ').filter(p => p);
+    if (parts.length <= 2) {
+      return parts.map(p => p[0].toUpperCase()).join('');
+    } else {
+      return (parts[0][0] + parts[parts.length - 1][0]).toUpperCase();
+    }
+  } 
+
+  
 }
+
+
