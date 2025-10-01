0e26ca7a19c7b606b095638bf5f1e1687a2f4fa9
KC-JulianKohn <julian.kohn@hotmail.de>
2025-10-01 10:48:05 +0200

    add tasks funktions

--- src/app/mainpage/tasks/tasks.ts
+++ src/app/mainpage/tasks/tasks.ts
@@ -1,19 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 import { TaskService } from '../../services/task-service';
 import { ContactService } from '../../services/contact-service';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { TaskInterface } from '../../interfaces/tasks.interface';
+import { Timestamp } from '@angular/fire/firestore';
 @Component({
   selector: 'app-tasks',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss'
 })
 export class Tasks {
   taskService = inject(TaskService)
   contactService = inject(ContactService);
-
   contactId: string | null = null;
+  subtaskTitle = '';
+  newTask: TaskInterface = {
+    title: '',
+    description: '',
+    due_date: Timestamp.now(),
+    priority: 'Low',
+    category: 'Technical Task',
+    stage: 'To do',
+    subtask: [],
+    assigned_to: []
+  };
 
   constructor() {
     if (this.contactService.contactsList.length > 0) {
@@ -21,7 +33,46 @@ export class Tasks {
     }
   }
 
-  get contact(): Contact | undefined {
-    return this.contactService.contactsList.find(c => c.id === this.contactId);
+  async onSubmit(form: NgForm) {
+    await this.taskService.addTask(this.newTask);
+    this.clearInputFields();
+    form.resetForm();
+  }
+
+  async deleteTask(taskId: string | undefined) {
+    if (!taskId) return;
+    await this.taskService.deleteTask(taskId);
+  }
+
+  clearInputFields() {
+    this.newTask = {
+      title: '',
+      description: '',
+      due_date: Timestamp.now(),
+      priority: '',
+      category: '',
+      stage: '',
+      subtask: [],
+      assigned_to: []
+    };
+    this.subtaskTitle = '';
+  }
+
+  addSubtask() {
+    this.newTask.subtask.push({ title: this.subtaskTitle, completed: false });
+    this.subtaskTitle = '';
+  }
+
+  removeSubtask(index: number) {
+    this.newTask.subtask.splice(index, 1);
+  }
+
+  toggleAssigned(contact: Contact, event: Event) {
+    const checked = (event.target as HTMLInputElement).checked;
+    if (checked) {
+      this.newTask.assigned_to.push(contact);
+    } else {
+      this.newTask.assigned_to = this.newTask.assigned_to.filter(c => c.id !== contact.id);
+    }
   }
 }
