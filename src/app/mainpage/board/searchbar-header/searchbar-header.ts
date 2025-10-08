70f39fd999fde4a6cd1aaac152fa03e812a1000d
Amer Alkhalidy <amer.alkhaldi@icloud.com>
2025-10-08 17:55:19 +0200

    add addResTask function

--- src/app/mainpage/board/searchbar-header/searchbar-header.ts
+++ src/app/mainpage/board/searchbar-header/searchbar-header.ts
@@ -1,39 +1,42 @@
 import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TaskService } from '../../../services/task-service';
-import { TaskInterface } from '../../../interfaces/tasks.interface';
-
-
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-searchbar-header',
   standalone: true,
-  imports: [FormsModule,],
+  imports: [FormsModule, RouterModule],
   templateUrl: './searchbar-header.html',
   styleUrls: ['./searchbar-header.scss']
 })
 export class SearchbarHeader {
-
-
   taskService = inject(TaskService);
+  router = inject(Router);
   searchTerm: string = '';
 
   @Output() searchResult = new EventEmitter<string>();
+  @Output() addTaskToStage = new EventEmitter<string>();
   @Output() add = new EventEmitter<string>();
 
   searchInput(): void {
-    this.searchResult.emit(this.searchTerm); 
+    this.searchResult.emit(this.searchTerm);
   }
 
   onSearch(event: Event): void {
     event.preventDefault();
-    // console.log(this.searchTerm);
-    this.searchInput(); 
+    this.searchInput();
   }
+
+  addResTask(stage: string) {
+    if (window.innerWidth >= 1080) {
+      this.addTaskToStage.emit(stage);
+    } else {
+      this.router.navigate(['/tasks']);
+    }
+  }
+
+  addTask(): void {
+  this.add.emit();
 }
-
-
-
-
-
-  
+}
