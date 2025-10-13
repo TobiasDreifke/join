2560a4562a261579f79d9c3da123b4f5882e5a19
Demis Moysidis <demis.moysidis@outlook.de>
2025-10-13 14:15:06 +0200

    feat: guard for checking if user is logged in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,16 @@ import { Tasks } from './mainpage/tasks/tasks';
 import { Board } from './mainpage/board/board';
 import { Login } from './start-screen/login/login';
 import { SignUp } from './start-screen/sign-up/sign-up';
+import { authFunctionalGuard } from './guards/auth-functional-guard';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/contacts', pathMatch: 'full'},
+    { path: '', redirectTo: '/contacts', pathMatch: 'full' },
     { path: 'legal', component: Legal },
     { path: 'privacy', component: Privacy },
-    { path: 'contacts', component: Contacts},
-    { path: 'summary', component: Summary },
-    { path: 'tasks', component: Tasks},
-    { path: 'board', component: Board},
-    { path: 'login', component: Login},
-    { path: 'sign-up', component: SignUp}
+    { path: 'contacts', component: Contacts, canActivate: [authFunctionalGuard] },
+    { path: 'summary', component: Summary, canActivate: [authFunctionalGuard] },
+    { path: 'tasks', component: Tasks, canActivate: [authFunctionalGuard] },
+    { path: 'board', component: Board, canActivate: [authFunctionalGuard] },
+    { path: 'login', component: Login },
+    { path: 'sign-up', component: SignUp }
 ];
