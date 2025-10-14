c833ae2819b20891f799a2760d855c82923e1b01
Demis Moysidis <demis.moysidis@outlook.de>
2025-10-14 11:41:01 +0200

    feat: reverse guard for logged user

---
+++ src/app/guards/reverse-auth-functional-guard-guard.ts
@@ -0,0 +1,14 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth-service';
+
+export const reverseAuthFunctionalGuardGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.logStatus()) {
+    return router.createUrlTree(['/summary']);
+  } else {
+    return true;
+  }
+};
