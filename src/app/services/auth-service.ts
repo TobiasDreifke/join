4ecab814ff576cf9802ee698183cbfbea9a1beec
Demis Moysidis <demis.moysidis@outlook.de>
2025-10-14 11:58:51 +0200

    feat: set guest login

--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -24,8 +24,8 @@ export class AuthService {
 
   async logout(){
     await signOut(this.auth);
-    this.logStatus.set(false);
     this.router.navigate(['/login']);
+    this.logStatus.set(false);
   }
 
   getDisplayName(){
