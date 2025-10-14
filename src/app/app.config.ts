887ae6b3fb215c28be3e5968af0b095db2c34ed8
Demis Moysidis <demis.moysidis@outlook.de>
2025-10-14 12:49:29 +0200

    feat: add transition to view changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -10,7 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), 
+    provideRouter(routes, withViewTransitions()), 
     provideFirebaseApp(() => initializeApp({ projectId: "join-e5bef", appId: "1:387219287108:web:b0b9a44f23a5141e8a2011", storageBucket: "join-e5bef.firebasestorage.app", apiKey: "AIzaSyCHwzQJ3d8q8bspT7nv86740Jz4OFdu9xg", authDomain: "join-e5bef.firebaseapp.com", messagingSenderId: "387219287108" })), 
     provideAuth(() => getAuth()), 
     provideFirestore(() => getFirestore())
