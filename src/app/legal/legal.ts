ab3a81e467ba20c03af2404899e8e6b0ce57ed9c
Tobias Dreifke <toobsecret@gmail.com>
2025-10-15 14:06:42 +0200

    added routing

--- src/app/legal/legal.ts
+++ src/app/legal/legal.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './legal.html',
   styleUrl: './legal.scss'
 })
