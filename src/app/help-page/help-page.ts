0df95e4bdb70b1cae2943622fdf976c959b2b661
KC-JulianKohn <julian.kohn@hotmail.de>
2025-10-16 00:45:31 +0200

    add legal- / privacy- / help-task

---
+++ src/app/help-page/help-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-help-page',
+  imports: [RouterLink],
+  templateUrl: './help-page.html',
+  styleUrl: './help-page.scss'
+})
+export class HelpPage {
+
+}
